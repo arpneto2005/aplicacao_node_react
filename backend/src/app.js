6340b07977351d579d2c67ce530ffa2adeacc1dc
Angelo Neto <arpneto2005@gmail.com>
2020-03-27 01:14:58 -0300

    Terminado BAckend e inicializado o Frontend

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const routes = require('./routes');
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+port = 3333;
+app.listen(port, () => {
+    console.log(`Server on, in port ${port}`)
+});
---
+++ backend/src/routes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const routes = express.Router();
+//const connection = require('./database/connection');
+
+const OngsController = require('./controllers/OngsController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+ /** Rotas Ongs */
+routes.post('/ongs', OngsController.create);
+routes.get('/ongs', OngsController.verOngs);
+
+/** rotas Incidents */
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.verIncidents);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+/** Rotas Profile */
+routes.get('/profile', ProfileController.verProfile);
+
+/** Rotas Session */
+routes.post('/session', SessionController.create);
+
+/**
+ * id: c970e4dd
+ */
+
+routes.get('/', async (resquest, response) => {
+    //const ongs = await connection('ongs').select('*')
+    return response.json({
+        name: "Ângelo"
+    });
+});
+
+module.exports = routes;
